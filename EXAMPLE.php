a0572918c6c7dd6c532781af0d4a7b76c03d8590
Devji Chhanga <dev.chh@gmail.com>
2014-03-18 12:28:36 +0530

    Create EXAMPLE.php

---
+++ EXAMPLE.php
@@ -0,0 +1,8 @@
+<?php
+	require('CAPTCHA.php');
+	session_start();
+	writeImage(220, 90, 4, 40, 45, "ITCKRIST.TTF", "255, 255, 255", "255, 100, 25");
+	
+	//Accessing MD5 of generated captcha for later verififcation
+	$MD5 = $_SESSION ['CaptchaCode'];
+?>
